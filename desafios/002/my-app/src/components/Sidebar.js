efd86971b0d188c06af3a73496d1f51223edc2e1
Murilo Messias <murilo.messias@gmail.com>
2021-09-03 17:51:30 -0300

    Add handleClick to Sidebar

--- desafios/002/my-app/src/components/Sidebar.js
+++ desafios/002/my-app/src/components/Sidebar.js
@@ -1,26 +1,27 @@
 import * as Title from './Title'
 
-const Sidebar = ({ posts, handleChangePost }) => (
-  <aside className="sidebar">
-    <Title.H3>Posts</Title.H3>
-    <nav>
-      <ul>
-        {posts.map(({ title, id }) => (
-          <li key={id} className="sidebar-item">
-            <a
-              href="/"
-              onClick={(e) => {
-                e.preventDefault()
-                handleChangePost(id)
-              }}
-            >
-              {title}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </aside>
-)
+const Sidebar = ({ posts, handleChangePost }) => {
+  const handleClick = (e, id) => {
+    e.preventDefault()
+    handleChangePost(id)
+  }
+
+  return (
+    <aside className="sidebar">
+      <Title.H3>Posts</Title.H3>
+      <nav>
+        <ul>
+          {posts.map(({ title, id }) => (
+            <li key={id} className="sidebar-item">
+              <a href="/" onClick={(e) => handleClick(e, id)}>
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  )
+}
 
 export default Sidebar
