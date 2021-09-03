79ccdf6eb65346ee0cdfd4a25bb4ff53f362afd2
Murilo Messias <murilo.messias@gmail.com>
2021-09-03 10:41:17 -0300

    Challenge 01 - Add Exercise 02

--- 001/my-app/src/App.js
+++ 001/my-app/src/App.js
@@ -1,3 +1,25 @@
-const App = () => <h1>App</h1>
+// Components
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Sidebar from './components/Sidebar'
+
+// Styles
+import './styles.css'
+
+const App = () => (
+  <div className="grid">
+    <Header title="Header" />
+    <Menu />
+
+    <div className="main">
+      <Sidebar />
+      <Content />
+    </div>
+
+    <Footer />
+  </div>
+)
 
 export default App
---
+++ 001/my-app/src/components/Content.js
@@ -0,0 +1,11 @@
+const Content = () => (
+  <main className="content">
+    <article>
+      <section>
+        <h1>Content</h1>
+      </section>
+    </article>
+  </main>
+)
+
+export default Content
