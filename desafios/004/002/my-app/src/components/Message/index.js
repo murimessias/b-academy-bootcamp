9a6c2d4af39c4a1f0140770eceb04bbe2c5e17db
Murilo Messias <murilo.messias@gmail.com>
2021-09-13 10:46:09 -0300

    Challenge 04 | 002 - Update Message component

--- desafios/004/002/my-app/src/components/Message/index.js
+++ desafios/004/002/my-app/src/components/Message/index.js
@@ -1,5 +1,5 @@
-const Message = ({ message }) => (
-  <div className="message-wrapper">{message}</div>
-)
+import * as S from './styles'
+
+const Message = ({ message }) => <S.Wrapper>{message}</S.Wrapper>
 
 export default Message
